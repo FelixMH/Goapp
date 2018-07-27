20e3bdfa65e4904b1dac97fcbba1862fd8e96865
FelixMH <felixmtzh.fm@gmail.com>
2018-07-27 16:50:17 -0500

    Conexión a la base de datos con GORM

--- 14server/05gorm/main.go
+++ 14server/05gorm/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
@@ -14,8 +16,9 @@ type Productos struct {
 }
 
 func main() {
-	db, err := gorm.Open("mysql", "developer:secret@/Goapp?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "dev:secret@/Goapp?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
+		log.Fatal(err)
 		panic("Error en la conexión a la base de datos")
 	}
 	defer db.Close()
